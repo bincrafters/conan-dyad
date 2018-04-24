b056d383e99895367e15a8cbea9bb1f82c4d26b6
Uilian Ries <uilianries@gmail.com>
2018-04-24 20:14:52 -0300

    Export symbols on windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -53,3 +53,5 @@ class DyadConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settins.os == "Windows":
+            self.cpp_info.libs.append("ws2_32")
